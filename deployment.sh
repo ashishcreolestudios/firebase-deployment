d3fe78d219aedb5d54bc27c1d65978239fddc138
= <=>
2019-02-01 13:09:51 +0530

    msg

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 # uniquify and sort the Xcode projct files
-yarn build
-firebase deploy --only hosting:beta
+# yarn build
+# firebase deploy --only hosting:beta
+
+echo "Creating a build to be deployed" &
+build= yarn build
+wait $build
+echo "Build generated" &
+deploy= firebase deploy --only hosting:beta
+wait $deploy
+echo "project deployed"
