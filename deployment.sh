8a9dd0f16d20b7345de5f51de1d4602b502df3eb
= <=>
2019-02-01 17:55:55 +0530

    commit message

--- deployment.sh
+++ deployment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Script to create build and deploy to beta
-echo "Creating optimized build that will be deployed on beta server...." &
-build= yarn build
-wait $build
-echo "Build is been generated... Deploying Project to beta..." &
-deploy= firebase deploy --only hosting:beta
-wait $deploy
-echo "Project is been deployed. pushing code to repository....."
+# echo "Creating optimized build that will be deployed on beta server...." &
+# build= yarn build
+# wait $build
+# echo "Build is been generated... Deploying Project to beta..." &
+# deploy= firebase deploy --only hosting:beta
+# wait $deploy
+# echo "Project is been deployed. pushing code to repository....."
